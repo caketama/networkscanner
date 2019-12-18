b5e4cd65ce9d5adca0dead6f9fbdd0f3891616da
cake <c4ke@m.me>
2019-12-18 17:10:56 -0500

    port reader working

--- port_reader.py
+++ port_reader.py
@@ -1,6 +1,16 @@
 import json
+from ports import port_numbers
 
-
-with open('ports.json', 'r') as file:
-    file = json.loads(file)
-    print(file['ports'])
+with open("ports.json.bak", "r") as file:
+    file = json.load(file)
+    for number in port_numbers:
+        if number in port_numbers:
+            number = str(number)
+            try:
+                port = file[number]["description"]
+                print(port)
+            except (KeyError, TypeError):
+                pass
+#        print(ports)
+#    description = file["443"]["description"]
+#    print(description)
