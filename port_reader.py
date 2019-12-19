5ccb4f26955cb47892c4be6c181625386a6c2fe4
cake <c4ke@m.me>
2019-12-19 10:37:51 -0500

    cleaning up

--- port_reader.py
+++ port_reader.py
@@ -1,7 +1,7 @@
 import json
 from ports import port_numbers
 
-with open("ports.json.bak", "r") as file:
+with open("ports.json", "r") as file:
     file = json.load(file)
     for number in port_numbers:
         if number in port_numbers:
