54af6005146f66d713193450edc81b25ba928a35
cake <c4ke@m.me>
2020-01-02 10:44:33 -0500

    re-organize files

--- src/flask_app/schema.py
+++ src/flask_app/schema.py
@@ -5,21 +5,11 @@ def schema(dbpath="scans.db"):
     with sqlite3.connect(dbpath) as conn:
         cursor = conn.cursor()
         DROPSQL = "DROP TABLE IF EXISTS {tablename};"
-        cursor.execute(DROPSQL.format(tablename="ips"))
-        sql = """ CREATE TABLE ips (
-                pk INTEGER PRIMARY KEY AUTOINCREMENT,
-                ip_address VARCHAR(15)
-            );"""
-        cursor.execute(sql)
-
         cursor.execute(DROPSQL.format(tablename="scans"))
         sql = """ CREATE TABLE scans (
-                pk INTEGER PRIMARY KEY AUTOINCREMENT,
-                ip_address_pk INTEGER,
+                ip_address VARCHAR(15),
                 ports VARCHAR,
-                services VARCHAR,
-                version_number VARCHAR,
-                FOREIGN KEY (ip_address_pk) REFERENCES ip_address(pk)
+                services VARCHAR
             );"""
         cursor.execute(sql)
 
--- src/flask_app/seed.py
+++ src/flask_app/seed.py
@@ -5,10 +5,7 @@ def seed(dbpath="scans.db"):
     with sqlite3.connect(dbpath) as conn:
         cursor = conn.cursor()
 
-        SQL = """INSERT INTO ips (pk, ip_address) VALUES ("1","127.0.0.1")"""
-        cursor.execute(SQL)
-
-        SQL = """ INSERT INTO scans (ip_address_pk, ports, services, version_number)VALUES ("1","22", "ssh", "1337")"""
+        SQL = """ INSERT INTO scans (ip_address, ports, services)VALUES ("127.0.0.1","22", "ssh")"""
         cursor.execute(SQL)
 
 
