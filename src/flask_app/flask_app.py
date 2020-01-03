797798c215ef05d21da973eaaa7a7170bb3997de
cake <jarrettw@protonmail.com>
2020-01-03 14:04:46 -0500

    subprocess scan via flask

--- src/flask_app/flask_app.py
+++ src/flask_app/flask_app.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+from subprocess import check_output
 
 app = Flask(__name__)
 
 
-@app.route('api/get_scans', methods=["GET"])
+@app.route("/api/scan", methods=["GET"])
+def scan():
+    ip = "192.168.56.4"
+    scan = check_output(["python", "port_scanner.py", ip]).decode()
+    return jsonify({"scan": scan})
+
+
+@app.route("/api/get_scans", methods=["GET"])
 def get_scans():
     pass
 
 
-@app.route('api/post_scans', methods=["POST"])
+@app.route("/api/post_scans", methods=["POST"])
 def post_scans():
     pass
 
 
-@app.route('api/add_scans', methods=["POST"])
+@app.route("/api/add_scans", methods=["POST"])
 def add_scans():
     pass
 
