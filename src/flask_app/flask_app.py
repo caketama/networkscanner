450b1fd4f17031550b73e6aef170a18ec64cf969
cake <jarrettw@protonmail.com>
2020-01-06 10:42:18 -0500

    remove scans refactor add scans

--- src/flask_app/flask_app.py
+++ src/flask_app/flask_app.py
@@ -6,15 +6,6 @@ from subprocess import check_output
 app = Flask(__name__)
 
 
-@app.route("/api/scan", methods=["POST"])
-def scan():
-    data = request.data
-    data = loads(data)
-    ip = data.get("ip_address")
-    scan = check_output(["python", "port_scanner.py", ip]).decode()
-    return jsonify({"scan": scan})
-
-
 @app.route("/api/get_scans", methods=["GET"])
 def get_scans():
     pass
@@ -27,7 +18,11 @@ def post_scans():
 
 @app.route("/api/add_scans", methods=["POST"])
 def add_scans():
-    pass
+    data = request.data
+    data = loads(data)
+    ip = data.get("ip_address")
+    scan = check_output(["python", "port_scanner.py", ip]).decode()
+    return jsonify({"scan": scan})
 
 
 if __name__ == "__main__":
