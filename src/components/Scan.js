c21f8685486ddf54c15e34b7bf64edd792003e7a
cake <jarrettw@protonmail.com>
2020-01-10 11:36:33 -0500

    scans render from db to front end

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -1,30 +1,27 @@
 import React, { useState } from "react";
 import { Text, Flex, Box } from "rebass";
 import { Input } from "@rebass/forms";
-import ScanResult from './ScanResult'
+import ScanResult from "./ScanResult";
 
 const Scan = props => {
   const [error, setError] = useState(false);
   const [scan, setScan] = useState([]);
-  const [ip, setIp] = useState("");
+  const [ip, setIp] = useState("192.168.56.4");
   const sendScan = async () => {
     setError(false);
     try {
       const endpoint = "http://localhost:5000/api/add_scan";
-      const data = { ip_address: ip }
-      console.log(data)
+      const data = { ip_address: ip };
       const configs = {
         method: "POST",
         body: JSON.stringify(data),
         mode: "cors",
-        headers: { 'Content-Type': 'application/json' }
+        headers: { "Content-Type": "application/json" }
       };
       const res = await fetch(endpoint, configs);
-      console.log(res)
       const ip_address = await res.json();
-      console.log(ip_address);
       if (ip_address) {
-        setIp(ip_address);
+        setScan(ip_address.scan);
       } else {
         setError(true);
       }
@@ -34,6 +31,9 @@ const Scan = props => {
     }
   };
 
+  let scanResult = scan.map(scans => {
+    return <p>{scans}</p>;
+  });
 
   return (
     <Flex>
@@ -44,7 +44,7 @@ const Scan = props => {
         <Input type="text" onChange={e => setIp(e.target.value)} />
         <button onClick={e => sendScan()}> SCAN </button>
         <Box width={1 / 3}></Box>
-        <ScanResult />
+    {scanResult}
       </Text>
     </Flex>
   );
